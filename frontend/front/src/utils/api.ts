b2ea5ab6f63c60314b9516788cb4371c0659818b
Иван <vano-smole@mail.ru>
2026-06-24 22:08:01 +0700

    add docker

--- frontend/front/src/utils/api.ts
+++ frontend/front/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: 'http://localhost:8000/api',
-  timeout: 10_000,
+  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
